5b3b620c5d07d7be5324c43068a5641fd66aae34
Nicolas-David <nicolas.david@unifesp.br>
2026-04-21 13:19:19 -0300

    Implement MonitoringFacade class with notification channels

--- alertDecorator.ts
+++ alertDecorator.ts
@@ -1,1 +1,27 @@
 //Aqui seria o decorator
+
+export class MonitoringFacade {
+  private sensors: Map<string, ISensor> = new Map();
+  private publisher: AlertPublisher;
+
+  constructor(private userRole: string = "operator") {
+    this.publisher = new AlertPublisher();
+
+    // Registra canais de notificação (Observer)
+    this.publisher.subscribe(new EmailChannel("ops@empresa.com"));
+    this.publisher.subscribe(new SMSChannel("+5511999990000"));
+    this.publisher.subscribe(new DashboardChannel());
+    this.publisher.subscribe(new WhatsAppChannel("+5511999990000"));
+  }
+
+   getLogs(): string[] {
+    return Logger.getInstance().getLogs();
+  }
+
+  readSensor(id: string): SensorReading {
+    const sensor = this.sensors.get(id);
+    if (!sensor) throw new Error(`Sensor não encontrado: ${id}`);
+    return sensor.read();
+  }
+}
+  
